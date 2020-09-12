22b3a1555015c9d6423f178d18b62b2d1ed0f80b
Solomon Ilori <solomonilori@gmail.com>
2020-09-12 09:47:22 +0100

    javascript variable

--- MamaT Restaurant/js/main.js
+++ MamaT Restaurant/js/main.js
@@ -5,6 +5,23 @@ function welcome(){
 }
 
 function addTheTwoNumber() {
-    document.getElementById('answer').value  = parseInt(document.getElementById('first_number').value) + parseInt(document.getElementById('second_number').value);
-    //document.getElementById('answer').value  = document.getElementById('first_number').value + document.getElementById('second_number').value;
+    var first_number;
+    var second_number;
+    var answer = 0;
+    
+    if (isNaN(document.getElementById('first_number').value)) {
+        alert('Kindly input number only');
+    } else {
+        first_number = parseInt(document.getElementById('first_number').value);
+        second_number = parseInt(document.getElementById('second_number').value);
+
+        answer = first_number + second_number;
+    }
+    
+    
+
+    console.log('first_number: ' + first_number);
+
+    document.getElementById('answer').value  =  answer;
+    
 }
